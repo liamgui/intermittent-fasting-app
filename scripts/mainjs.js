959e55983768a21000f16ebee08ad94d8927ff92
Will Desktop <guidkid3@gmail.com>
2018-06-10 22:48:34 -0400

    semi-working timer- it counts, but need to store date/time of start and stop in localStorage. Timer javascript commit

--- scripts/mainjs.js
+++ scripts/mainjs.js
@@ -10,6 +10,39 @@ var graph5 = document.getElementById("graph5");
 var graph6 = document.getElementById("graph6");
 var graph7 = document.getElementById("graph7");
 
+var button = document.getElementById("button");
+var startWord = document.getElementById("startWord");
+var currentCount = 0;
+var counting = false;
+var interval;
+
+
+
+function buttonPress() {
+    if (counting === false) {
+        console.log("started");
+        interval = setInterval(count, 1000);
+        function count() {
+            ++currentCount;
+            console.log(currentCount);
+            timer.innerHTML = "0:00:0" + currentCount;
+            timer.style.textAlign = "center";
+            timer.style.fontFamily = "comfortaalight";
+            timer.style.color = "#ea7600";
+            timer.style.fontSize = "2.8em";
+            timer.style.letterSpacing = "0.08em" ;
+            timer.style.marginTop = "0.4em";
+        }
+        startWord.innerHTML = "Stop";
+        counting = true;
+    } else if (counting === true){
+        clearInterval(interval);
+        counting = false;
+        startWord.innerHTML = "Start";
+    }
+        
+
+}
 
 
 
@@ -18,10 +51,14 @@ var graph7 = document.getElementById("graph7");
 
 
 
-
+//INIT FUNCTION
 
 function init() {
-
+    if (button.addEventListener) {
+        button.addEventListener("click", buttonPress, "false");
+    } else if (button.attachEvent) {
+        button.attachEvent("onclick", buttonPress);
+    }
 }
 
 
